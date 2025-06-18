789d5ce3eff983bdbbed644ef1928d5a1f502308
Gustavo Nicolas Gutierrez <73355703+NicolasGutierrez1992@users.noreply.github.com>
2025-06-18 10:47:53 -0300

    Update Order.js

--- server/src/models/Order.js
+++ server/src/models/Order.js
@@ -1,3 +1,4 @@
+const { Combine } = require("lucide-react");
 const pool = require("../config/db");
 
 const getOrderByIdUser = async (idClient) => {
@@ -12,11 +13,23 @@ const createOrder = async (orderData) => {
   
   //Obtenemos el ultimo id_pedido para asignar el nuevo id_pedido
   const lastOrderQuery = "SELECT id_pedido FROM pedido ORDER BY id_pedido DESC LIMIT 1";
-    const lastOrderResult = await pool.query(lastOrderQuery);
-    const lastOrderId = lastOrderResult.rows[0] ? lastOrderResult.rows[0].id_pedido : 0;
-    const newOrderId = lastOrderId + 1; // Incrementamos el último ID para el nuevo pedido
-    
-    const { id_Client, cantidaditems, fecharecepcion, estado, comentario } = orderData;
+  console.log("Consultando el último ID de pedido...");
+  console.log("Consulta SQL:", lastOrderQuery);
+  // Ejecutamos la consulta para obtener el último ID de pedido  
+  const lastOrderResult = await pool.query(lastOrderQuery);
+  console.log("Resultado de la consulta del último ID de pedido:", lastOrderResult.rows);
+  const lastOrderId = lastOrderResult.rows[0] ? lastOrderResult.rows[0].id_pedido : 0;
+  console.log("Último ID de pedido encontrado:", lastOrderId);
+  const newOrderId = lastOrderId + 1; // Incrementamos el último ID para el nuevo pedido
+  console.log("Nuevo ID de pedido asignado:", newOrderId);
+  const { id_Client, cantidaditems, fecharecepcion, estado, comentario } = orderData;
+  console.log("Datos del nuevo pedido:", {
+    id_Client,
+    cantidaditems,
+    fecharecepcion,
+    estado,
+    comentario
+  });
 
 
   const query = `
@@ -24,7 +37,16 @@ const createOrder = async (orderData) => {
     VALUES ($1, $2, $3, $4 , $5, %6)
     RETURNING *
   `;
-
+console.log("Consulta SQL para insertar el nuevo pedido:", query);
+  console.log("Valores a insertar:", {
+    newOrderId,
+    id_Client,
+    cantidaditems,
+    fecharecepcion,
+    estado,
+    comentario
+  });
+  
   const values = [newOrderId, id_Client, cantidaditems, fecharecepcion, estado , comentario];
   const result = await pool.query(query, values);
   // Devolvemos el nuevo pedido creado
